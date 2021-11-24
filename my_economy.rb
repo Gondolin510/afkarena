52a54ade5a66a2a93926c9edace57722657e8623
Gondolin <ploumplamplim@gmail.com>
2021-11-24 09:07:42 +0100

    my_economy

---
+++ my_economy.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require './economy'
+
+s=Simulator.new do
+  #at rc 559: the amount required to level up (xp and gold are in K)
+  @hero_level=359
+  @stage="37-16"
+  @afk_xp=13235
+  @afk_gold=844
+
+  @player_level=165
+  @vip=9
+  @subscription=true
+
+  #dia 5 at floor 360
+  @tr_twisted ||=257
+  @tr_poe ||=905
+
+  @gh_team_wrizz_gold = 948
+  @gh_team_soren_gold = 978
+  @gh_team_wrizz_coin = 1058
+
+  @gh_wrizz_chests = 21
+  @gh_wrizz_gold = 1897
+  @gh_soren_gold = 1957
+
+  @noble_coe = get_coe(:cores)
+  @dura_nb_selling =1
+
+  @tower_god = 200
+
+  @misty = get_misty(misty_guild_twisted: :guild, misty_purple_blue: :blue)
+end
+s.summary
+# p s.real_afk_gold, s.real_afk_xp
+# require 'pry'; binding.pry
+s.show_variables(verbose: true)
