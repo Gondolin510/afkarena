722484abf88f64b9ec4a1c3c32a9aeb78cedcb33
Gondolin <ploumplamplim@gmail.com>
2021-11-26 13:01:40 +0100

    Results

--- my_economy.rb
+++ my_economy.rb
@@ -28,8 +28,6 @@ s=Simulator.new do
   @tower_kt = 561
   @tower_4f = [296, 291, 361, 393] #lb, maulers, wilders, gb
   @tower_god = [143, 231] #celo, hypo
-  @monthly_levelup=10
-  #set_tower_progression(10)
 
   @garrison=true
   @dim_exchange=true
@@ -37,6 +35,21 @@ s=Simulator.new do
   @misty = get_misty(guild_twisted: :guild)
 
   @shop_items = get_shop_items(:dust_h)
+  @monthly_levelup=10
+  #set_tower_progression_from_levelup(10)
+
+=begin
+With set_tower_progression_from_levelup(10) and @monthly_levelup=0
+  - Not buying dust:
+  level: 2.9 days (10.35 by month) {gold: 2.11 days, xp: 1.81 days, dust: 2.9 days} [monthly remains: 88005.18 gold + 1030671.08 xp]
+  - Buying dust:
+  level: 2.19 days (13.7 by month) {gold: 2.11 days, xp: 1.81 days, dust: 2.19 days} [monthly remains: 11821.59 gold + 479300.28 xp]
+With @monthly_levelup=10
+  - Not buying dust:
+  level: Infinity days (0 by month) {gold: 9.42 days, xp: 5.47 days, dust: -43.48 days} [monthly remains: 72329.45 gold + 901825.95 xp + -21622.92 dust]
+  - Buying dust:
+  level: 11.27 days (2.66 by month) {gold: 9.42 days, xp: 5.47 days, dust: 11.27 days} [monthly remains: 11822 gold + 463909.59 xp]
+=end
 end
 
 if __FILE__ == $0
