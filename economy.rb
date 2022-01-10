73f17f01a49d801eadcf6c62b2747b1ae7d3458c
Gondolin <ploumplamplim@gmail.com>
2022-01-10 14:39:28 +0100

    TODO++

--- economy.rb
+++ economy.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #TODO: more events?, board level<7 + team quests
-#TODO: finish yuexi, T1/T2 conversion, refined summonings (ie by faction), campaign progression (quests)
+#TODO: finish yuexi, T1/T2 conversion, refined summonings (ie by faction), campaign progression (quests), timegazers
 
 require './value'
 require 'json'
